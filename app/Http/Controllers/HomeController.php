bcc6e30ad26a0fcc9d55842047bada17d1067997
Della Arselatuiqrom <75599380+Dellarselatuiqrom@users.noreply.github.com>
2021-06-02 11:30:17 +0700

    membuat desain homepage, dasboard admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $data = array('title' => 'Homepage');
+        return view('home.index', $data);
+    }
+    public function about() {
+        $data = array('title' => 'Tentang Kami');
+        return view('home.about', $data);
+    }
+
+    public function kontak() {
+        $data = array('title' => 'Kontak Kami');
+        return view('home.kontak', $data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'HomeController@index');
+Route::get('/about', 'HomeController@about');
+Route::get('/kontak', 'HomeController@kontak');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/', 'DashboardController@index');
+  });
+//Route::get('/', function () {
+   // return view('welcome');
+//});
