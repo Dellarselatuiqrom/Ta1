c50f8ac944aec8bcd4ace8c0d7d84a8c13f1a0fb
Della Arselatuiqrom <75599380+Dellarselatuiqrom@users.noreply.github.com>
2021-06-03 12:38:06 +0700

    edit controller

--- app/Http/Controllers/AlamatPengirimanController.php
+++ app/Http/Controllers/AlamatPengirimanController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\AlamatPengiriman;
+use App\Models\AlamatPengiriman;
 
 class AlamatPengirimanController extends Controller
 {
